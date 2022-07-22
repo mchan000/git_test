87b54fbe076af25ac82f2b92b150ebf1a13a3708
mchan000 <chan1846@umn.edu>
2022-07-21 23:25:17 -0500

    Implemented start and pause buttons. Added study count down finish sound

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 
 let paused = true
+let breakTime = false
+let difference;
+let minutes, seconds;
 
-let difference = 1500
+if(breakTime = false){
+    difference = 1500;
+}
+else{
+    difference = 300;
+}
 
 let startBtn = document.querySelector('#start');
 let pauseBtn = document.querySelector('#pause');
+let studyOver = new Audio('study_over.mp3');
+let breakOver = new Audio('break_over.mp3');
 
 let countDown;
 
 function pauseCount(){
     paused = true;
     clearInterval(countDown);
+    countDown = 0;
 }
 
 function startCount(){
     paused = false;
-    if(paused == false){
+    if(countDown){
+
+    }
+    else if(paused == false){
+        clearInterval(countDown);
         countDown = setInterval(function(){
             difference = difference - 1;
     
@@ -30,16 +45,17 @@ function startCount(){
             }
     
             document.getElementById("countdown").innerHTML = minutes + ":" + seconds
+            document.title = minutes + ":" + seconds;
     
             if (difference < 0){
                 clearInterval(countDown);
+                studyOver.play();
                 document.getElementById("countdown").innerHTML = "Break time";
+                document.title = "Florodoro"
             }
         }, 1000);
     }
 }
-
-
 
 startBtn.addEventListener('click', startCount);
 pauseBtn.addEventListener('click', pauseCount);
